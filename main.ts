6ca8133cc2d543c0646c8efaef8aea3436c2bce0
Egyed József <jegyed50@gmail.com>
2023-05-31 07:35:01 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-//Earthquake Detector
+/**
+ * Earthquake Detector
+ */
+let s = 0
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    s = input.acceleration(Dimension.Strength)
+    s = s - 500
+    serial.writeValue("s", s)
+    led.plotBarGraph(
+    s,
+    3081
+    )
+    basic.pause(100)
+})
